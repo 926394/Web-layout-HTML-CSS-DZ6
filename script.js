840034cf59ad20ba8e95aed627a00392f8fee1f9
Nikolay <meta1mang@mail.ru>
2024-07-11 20:05:31 +0300

    code_mainMenu_DZ-10.2

--- script.js
+++ script.js
@@ -6,5 +6,10 @@ const iconXmenu = Clothing_store.querySelector('.iconXmenu');
 // иконка меню крестик
 
 function toggleMenu() {
-  main1Menu.classList.toggle()
+  main1Menu.classList.toggle('hidden');
 }
+// функция скрывает или показывает меню сайта
+
+iconMenu.addEventListener('click',toggleMenu);
+// по клику на иконку срабатывает функция
+iconXmenu.addEventListener('click',toggleMenu);
