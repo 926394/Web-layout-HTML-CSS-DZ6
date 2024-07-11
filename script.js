bcb07dddd31c1bea6d6658510b1a0a0e9203ca69
Nikolay <meta1mang@mail.ru>
2024-07-11 19:56:20 +0300

    code_mainMenu_DZ-10.1

---
+++ script.js
@@ -0,0 +1,10 @@
+const main1Menu = Clothing_store.querySelector('.main1Menu'); 
+// меню сайта
+const iconMenu = Clothing_store.querySelector('.iconMenu'); 
+// иконка меню сайта
+const iconXmenu = Clothing_store.querySelector('.iconXmenu'); 
+// иконка меню крестик
+
+function toggleMenu() {
+  main1Menu.classList.toggle()
+}
