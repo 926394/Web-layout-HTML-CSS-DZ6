786d4d4d5924705ab5e396798ccea7ec4025d21e
Nikolay <meta1mang@mail.ru>
2024-07-12 10:38:38 +0300

    hidden_MenuDZ-10.3

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const main1Menu = Clothing_store.querySelector('.main1Menu'); 
+const mainBurMenu = document.querySelector('.mainBurMenu'); 
 // меню сайта
-const iconMenu = Clothing_store.querySelector('.iconMenu'); 
+const iconMenu = document.querySelector('.iconMenu'); 
 // иконка меню сайта
-const iconXmenu = Clothing_store.querySelector('.iconXmenu'); 
+const iconXmenu = document.querySelector('.iconXmenu'); 
 // иконка меню крестик
 
-function toggleMenu() {
-  main1Menu.classList.toggle('hidden');
+function hiddenMenu() {
+  mainBurMenu.classList.toggle('hidden');
 }
 // функция скрывает или показывает меню сайта
 
-iconMenu.addEventListener('click',toggleMenu);
+iconMenu.addEventListener('click',hiddenMenu);
 // по клику на иконку срабатывает функция
-iconXmenu.addEventListener('click',toggleMenu);
+iconXmenu.addEventListener('click',hiddenMenu);
